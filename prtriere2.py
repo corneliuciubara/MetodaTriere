fd42c2a00d561f50c54ad6ceaca13603b0b028c9
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-12-05 18:20:21 +0200

    Update prtriere2.py

--- prtriere2.py
+++ prtriere2.py
@@ -1,1 +1,27 @@
+a = int(input("a = "))
+b = int(input("b = "))
 
+# Transformam variabilele integer in liste cu stringuri
+lsta = list(str(a)) 
+lstb = list(str(b))
+
+# Afisam listele
+print(lsta)
+print(lstb)
+
+# Parcurgem lista a si verificam daca elementele din a sunt in b
+rezult = True
+for i in range(0, len(lsta)):
+    if not (lsta[i] in lstb) :
+        rezult = False
+        break
+
+# Parcurgem lista b si verificam daca elementele din b sunt in a
+for i in range(0, len(lstb)):
+    if not (lstb[i] in lsta) :
+        rezult = False
+        break
+
+# Afisam rezultatul, daca numerele sunt prietene, afisaza "True"
+# Daca nu sunt prietene, afiseaza "False" 
+print(rezult)  
